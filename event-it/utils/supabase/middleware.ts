2825d2b01c624b9a56209c5250dd9ba89e92240a
Lennard Wilkens <l.wilkens@deos-ag.com>
2025-05-28 14:26:49 +0200

    Added Shadcn and auth flow

--- event-it/utils/supabase/middleware.ts
+++ event-it/utils/supabase/middleware.ts
@@ -39,13 +39,8 @@ export const updateSession = async (request: NextRequest) => {
     // https://supabase.com/docs/guides/auth/server-side/nextjs
     const user = await supabase.auth.getUser();
 
-    // protected routes
     if (!request.nextUrl.pathname.includes("/sign-in") && !request.nextUrl.pathname.includes("/sign-up") && user.error) {
       return NextResponse.redirect(new URL("/sign-in", request.url));
-    }
-
-    if (request.nextUrl.pathname === "/" && !user.error) {
-      return NextResponse.redirect(new URL("/protected", request.url));
     }
 
     return response;
