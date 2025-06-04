253120b2641c0b2e987e5691962b02bea65177ab
Lennard Wilkens <lennard.wilkens.2408@gmail.com>
2025-06-04 14:23:56 +0200

    done

--- event-it/app/layout.tsx
+++ event-it/app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import {Toaster} from "@/components/ui/toaster";
 import ToastFromQuery from "@/components/toast/toastFromQuery";
 import {Suspense} from "react";
+import {PopupProvider} from "@/components/provider/popupProvider";
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
@@ -26,8 +27,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={geistSans.className} suppressHydrationWarning>
-      <body className="">
-      {children}
+      <body className="w-screen">
+      <PopupProvider>
+        {children}
+      </PopupProvider>
       <Suspense fallback={null} >
         <ToastFromQuery/>
       </Suspense>
