cb3c78956a3409f7cbd0bb6ba6035b66bbc09a56
Lennard Wilkens <lennard.wilkens.2408@gmail.com>
2025-06-06 14:31:29 +0200

    donehjtrnjrzjrkuilzluzfgrjpi

--- event-it/app/(app)/friends/groups/layout.tsx
+++ event-it/app/(app)/friends/groups/layout.tsx
@@ -2,9 +2,9 @@ import FriendGroupTop from "@/components/friends/friendGroupTop";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
-        <div className="pt-10 px-4 max-w-screen">
+        <div className="px-4 max-w-screen mt-2">
             <FriendGroupTop/>
-            <div className={"w-full border-t border-2 my-2"}></div>
+            <div className={"w-full border-t border-2 my-4"}></div>
             {children}
         </div>
     );
